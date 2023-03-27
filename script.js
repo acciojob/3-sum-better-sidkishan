4c9de7ec7106c529cb6669dc5a75479cdc543b3e
Siddharth Kishan <91934415+sidkishan@users.noreply.github.com>
2023-03-27 20:21:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 function threeSum(arr, target) {
-// write your code here
-  
+
+  arr.sort();
+	let closestSum = Infinity;
+	for(var i = 0; i < arr.length - 2; i++){
+		// now apply 2 sum here
+		var left = i + 1;
+		var right = arr.length - 1;
+		while(left < right){
+			var sum = arr[i] + arr[left] + arr[right];
+			if(Math.abs(target - closestSum) >= Math.abs(sum - closestSum)){
+				closestSum = sum;
+			}
+			if(sum > target){
+				right--;
+			}
+			else{
+				left++;
+			}
+		}
+	}
+	return closestSum;
 }
 
 module.exports = threeSum;
