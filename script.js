6845417b654328007fce8de233fecbb416fe62cb
Siddharth Kishan <91934415+sidkishan@users.noreply.github.com>
2023-05-16 00:35:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -19,4 +19,28 @@ function threeSum(arr, target) {
 	return ans
 }
 
+	// arr.sort((a, b) => a - b);
+	// let closestSum = Infinity;
+	// for(var i = 0; i < arr.length - 2; i++){
+	// 	// now apply 2 sum here
+	// 	var left = i + 1;
+	// 	var right = arr.length - 1;
+	// 	while(left < right){
+	// 		var sum = arr[i] + arr[left] + arr[right];
+	// 		if(Math.abs(closestSum-target) > Math.abs(sum - target)){
+	// 			closestSum = sum;
+	// 		}
+	// 		if(sum > target){
+	// 			right--;
+	// 		}
+	// 		else{
+	// 			left++;
+	// 		}
+	// 	}
+	// }
+	// return closestSum;
+
+
+
+
 module.exports = threeSum;
