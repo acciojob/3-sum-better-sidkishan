17ce3bc46820fa3d893f318837d0f7b57f303ae9
Siddharth Kishan <91934415+sidkishan@users.noreply.github.com>
2023-05-16 00:31:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ function threeSum(arr, target) {
 	arr.sort((a, b) => a - b);
 	var ans = Infinity;
   for(let i = 0; i < arr.length - 2; i++){
-	  let newTarget = target - arr[i];
+	  //let newTarget = target - arr[i];
 	  let p = i + 1, q = arr.length - 1;
 	  while(p < q){
-		  let temp = arr[p]+ arr[q]
-		  let diff = Math.max(ans, newTarget) - Math.min(ans, newTarget)
-		  let currDiff = Math.max(temp, newTarget) - Math.min(temp, newTarget)
+		  let temp = arr[p]+ arr[q] + arr[i]
+		  let diff = Math.max(ans, target) - Math.min(ans, target)
+		  let currDiff = Math.max(temp, target) - Math.min(temp, target)
 		  if(currDiff <diff){
-			  ans = arr[i] + temp;
+			  ans = temp;
 		  }
 		  if(temp > newTarget){
 			  q--
